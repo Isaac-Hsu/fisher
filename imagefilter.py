5611b68cc5c3c55d7888172dfcc4c7d06b8d1527
Isaac <isaac.k.hsu@gmail.com>
2026-03-14 00:56:14 -0700

    layers numbers slight change

--- imagefilter.py
+++ imagefilter.py
@@ -1,3 +1,4 @@
+# takes in raw fishing image, returns processed image
 # takes ~0.01-0.03s
 
 import cv2
@@ -5,19 +6,19 @@ import numpy as np
 
 def imgfilter(img): 
     greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    lo = 54
-    hi = 55
-
+    
+    lo = 55
+    hi = 56
+    
     layer = (greyscale.astype(np.float32) - lo) / (hi - lo) * 255
     layer = np.clip(layer, 0, 255).astype(np.uint8)
-
+    
     kernel_small = np.ones((2, 2), np.uint8)
     noise = cv2.morphologyEx(layer, cv2.MORPH_OPEN, kernel_small, iterations=1)
-
+    
     kernel_close = np.ones((2, 2), np.uint8)
     filled = cv2.morphologyEx(noise, cv2.MORPH_CLOSE, kernel_close, iterations=1) # fills small holes or something
-
+    
     _, binary = cv2.threshold(filled, 230, 255, cv2.THRESH_BINARY)
-
+    
     return binary
